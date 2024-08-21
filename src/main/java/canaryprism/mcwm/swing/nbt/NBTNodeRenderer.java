f4421f9573f20ee51cb7b12033ecd9aef45e4156
Canary-Prism <canaryprsn@gmail.com>
2024-08-22 03:59:42 +0800

    icons. lots of them

--- src/main/java/canaryprism/mcwm/swing/nbt/NBTNodeRenderer.java
+++ src/main/java/canaryprism/mcwm/swing/nbt/NBTNodeRenderer.java
@@ -1,5 +1,18 @@
 package canaryprism.mcwm.swing.nbt;
 
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executors;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JComponent;
 import javax.swing.JTree;
 import javax.swing.tree.DefaultMutableTreeNode;
@@ -10,6 +23,7 @@ import net.querz.nbt.tag.ByteArrayTag;
 import net.querz.nbt.tag.ByteTag;
 import net.querz.nbt.tag.CompoundTag;
 import net.querz.nbt.tag.DoubleTag;
+import net.querz.nbt.tag.EndTag;
 import net.querz.nbt.tag.FloatTag;
 import net.querz.nbt.tag.IntArrayTag;
 import net.querz.nbt.tag.IntTag;
@@ -21,6 +35,38 @@ import net.querz.nbt.tag.StringTag;
 import net.querz.nbt.tag.Tag;
 
 public class NBTNodeRenderer extends DefaultTreeCellRenderer {
+
+    private final HashMap<String, Image> icons = new HashMap<>();
+    {
+        var icon_resource = NBTNodeRenderer.class.getResource("/mcwm/nbt/");
+        
+        try {
+            var icon_path = Path.of(icon_resource.toURI());
+            
+            
+            try (var ex = Executors.newVirtualThreadPerTaskExecutor();
+                var stream = Files.list(icon_path)) {
+
+                var futures = stream.map((path) -> CompletableFuture.runAsync(() -> {
+                    try (var is = Files.newInputStream(path)) {
+                        var image = ImageIO.read(is);
+                        synchronized (icons) {
+                            icons.put(path.getFileName().toString().split("\\.")[0], image);
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                }, ex)).toArray(CompletableFuture[]::new);
+
+                CompletableFuture.allOf(futures).join();
+
+            }
+
+        } catch (URISyntaxException | IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     @Override
     public JComponent getTreeCellRendererComponent(
@@ -46,6 +92,95 @@ public class NBTNodeRenderer extends DefaultTreeCellRenderer {
         };
 
         setText(str);
+
+        var buffered_image = new BufferedImage(25, 25, BufferedImage.TYPE_INT_ARGB);
+
+        var g = buffered_image.createGraphics();
+
+        {
+
+            var o = node.getUserObject();
+            boolean named = false;
+            if (o instanceof NamedTag named_tag) {
+                o = named_tag.getTag();
+                named = true;
+            }
+            var tag = o;
+
+            //#region draw tag if is tag
+            {
+                var copy = tag;
+                if (copy instanceof ListTag<?> list_tag) {
+                    try {
+                        if (list_tag.getTypeClass() != EndTag.class) {
+                            copy = list_tag.getTypeClass().getDeclaredConstructor().newInstance();
+                        }
+                    } catch (InstantiationException | IllegalAccessException | IllegalArgumentException
+                            | InvocationTargetException | NoSuchMethodException | SecurityException e) {
+                        e.printStackTrace();
+                    }
+                }
+                if (copy instanceof Tag && !(copy instanceof CompoundTag) && !(copy instanceof ListTag<?>)) {
+                    g.drawImage(icons.get("tag"), 0, 0, null);
+                }
+            }
+            //#endregion
+    
+            //#region draw the type
+            {
+                var copy = tag;
+                if (copy instanceof ListTag<?> list_tag) {
+                    try {
+                        if (list_tag.getTypeClass() != EndTag.class) {
+                            copy = list_tag.getTypeClass().getDeclaredConstructor().newInstance();
+                        } else {
+                            copy = new Object();
+                        }
+                    } catch (InstantiationException | IllegalAccessException | IllegalArgumentException
+                            | InvocationTargetException | NoSuchMethodException | SecurityException e) {
+                        e.printStackTrace();
+                    }
+                }
+    
+                var type = switch (copy) {
+                    case ByteTag _ -> "byte";
+                    case ShortTag _ -> "short";
+                    case IntTag _ -> "int";
+                    case LongTag _ -> "long";
+                    case FloatTag _ -> "float";
+                    case DoubleTag _ -> "double";
+                    case ByteArrayTag _ -> "byte_array";
+                    case StringTag _ -> "string";
+                    case ListTag<?> _ -> "list";
+                    case CompoundTag _ -> "compound";
+                    case IntArrayTag _ -> "int_array";
+                    case LongArrayTag _ -> "long_array";
+    
+                    case Byte _ -> "byte";
+                    case Integer _ -> "int";
+                    case Long _ -> "long";
+                    default -> null; // should never happen
+                };
+    
+                if (type != null)
+                    g.drawImage(icons.get(type), 0, 0, null);
+            }
+            //#endregion
+    
+            //#region draw list bracket if is list
+            if (tag instanceof ListTag<?>) {
+                g.drawImage(icons.get("list_container"), 0, 0, null);
+            }
+            //#endregion
+    
+            //#region draw named tag if is named tag
+            if (named) {
+                g.drawImage(icons.get("named"), 0, 0, null);
+            }
+            //#endregion
+        }
+
+        setIcon(new ImageIcon(buffered_image));
 
         return this;
     }
