32aeac37c874699b7c23bcddc1128ab51d456850
Canary-Prism <canaryprsn@gmail.com>
2024-08-23 22:26:27 +0800

    you didn't see that

--- src/main/java/canaryprism/mcwm/Main.java
+++ src/main/java/canaryprism/mcwm/Main.java
@@ -96,7 +96,7 @@ import static java.nio.file.StandardWatchEventKinds.*;
 public class Main {
 
 
-    public static final String VERSION = "2.4.4-beta-1";
+    public static final String VERSION = "2.4.4";
 
     private static boolean hasArg(String[] args, String arg) {
         return Stream.of(args).anyMatch(e -> e.equals(arg));
