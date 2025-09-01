7a582aee29512818b7c5f47b7b0ad9692038ce4e
Canary-Prism <canaryprsn@gmail.com>
2025-09-01 21:02:31 +0800

    add module

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module canaryprism.mcwm {
+    requires com.formdev.flatlaf;
+    requires dev.dirs;
+    requires java.datatransfer;
+    requires java.desktop;
+    requires NBT;
+    requires org.apache.commons.lang3;
+    requires org.apache.commons.text;
+    requires org.json;
+}
